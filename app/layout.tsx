9d8c38c90f0429cd57bdcc6b606a13593e8eadd9
Prakash Chandra Awal <prakashchawal@Prakashs-MacBook-Pro.local>
2024-12-20 08:48:00 +1100

    fake update 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <h1>FUCK YOU !!!</h1>
+        <h1>FUCK YOU FISHBOWL !!!</h1>
         {/* <Navbar />
         <main className="flex-grow">{children}</main>
         <Toaster />
